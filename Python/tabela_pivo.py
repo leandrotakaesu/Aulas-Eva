cd1e0531f57111e30806e4fd2fcaa3821e3543d4
evacosta <evacosta.if@gmail.com>
2025-07-19 09:41:53 -0300

    explicacao tabela pivo

--- Python/tabela_pivo.py
+++ Python/tabela_pivo.py
@@ -8,4 +8,12 @@ dados = {
 df_vendas = pd.DataFrame(dados)
 
 # Qual foi o valor total de vendas (sum) de cada Categoria em cada Cidade?
+tabela_pivo = df_vendas.pivot_table(
+    index='Cidade',# coluna cujos valores virarão LINHAS
+    columns='Categoria', # coluna cujos valores virarão COLUNAS
+    values='ValorVenda',# coluna com os numeros que queremos calcular
+    aggfunc='sum',#  A operação que queremos fazer
+    fill_value=0# (Opcional) Preenche células vazias (NaN) com 0
+)
 
+print(tabela_pivo)
